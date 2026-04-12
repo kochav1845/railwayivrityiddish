294f40370cf1746c2e31a2d5578d071f8acb63b8
kochav1845 <a88933513@gmail.com>
2026-04-12 14:21:03 -0400

    Updated server.py

--- railway/server.py
+++ railway/server.py
@@ -97,5 +97,5 @@ async def transcribe(audio: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
